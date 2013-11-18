c2362a7fefa4d298e1b3a88caeba2a1aca9f6a06
Daniel Torok <daniel.torok@prezi.com>
2013-11-18 01:59:31 +0100

    cleanup

--- tiles.hs
+++ tiles.hs
@@ -1,11 +1,11 @@
-import Control.Lens
+import Control.Lens hiding (transform)
 
 
 -------------
 -- data types
 -------------
 
-data Color = White | Yellow | Red deriving (Show)
+data Color = White | Yellow | Red deriving (Show, Eq)
 data Wall = Wall {
 			width  :: Int,
 			height :: Int,
@@ -30,6 +30,12 @@ setColor x y c wall = wall{tiles=tiles'}
 	where
 		tiles' = (tiles wall) & element y .~ row'
 		row' = ((tiles wall) !! y) & element x .~ Just c
+
+getColor :: (Int, Int) -> Wall -> Maybe Color
+getColor (x, y) wall = (tiles wall) !! y !! x
+
+isEmptyTitle :: Wall -> (Int, Int) -> Bool
+isEmptyTitle wall coord = getColor coord wall == Nothing
 
 wall2html :: Wall -> String
 wall2html wall = mkhtml $ (mktable . unwords) htmlrows
@@ -60,17 +66,19 @@ wall2html wall = mkhtml $ (mktable . unwords) htmlrows
 add :: Shape -> Int -> Int -> Int -> Color -> Wall -> Wall
 add shape x y r c wall = wall'
 	where
-		shape' = (translate x y) . (rotate r) $ shape
+		shape' = transform x y r shape
 		wall' = foldl setTile wall shape'
 		setTile w (sx, sy) = setColor sx sy c w
 
-addSafe :: Shape -> Int -> Int -> Int -> Color -> Wall -> Maybe Wall
-addSafe shape x y r c wall = case isEmpty of
+addSafe :: Shape -> Int -> Int -> Int -> Color -> Maybe Wall -> Maybe Wall
+addSafe _ _ _ _ _ Nothing = Nothing
+addSafe shape x y r c (Just wall) = case isClear of
 								True  -> Just (add shape x y r c wall)
-								False -> Nothing
+								False -> Just wall
 	where
-		isEmpty = undefined							
-
+		shape' = transform x y r shape
+		isClear = (filter (hasTitleAlready wall) shape') == []
+		hasTitleAlready wall coord = not $ isEmptyTitle wall coord
 
 ---------
 -- shapes
@@ -79,13 +87,22 @@ addSafe shape x y r c wall = case isEmpty of
 rotate :: Int -> Shape -> Shape
 rotate r shape = map rotateCoord shape
 	where 
-		rotateCoord coord = foldl rotateOnce coord [1..r]
+		norm :: Int -> Int
+		norm r | r > 3 = r `mod` 4
+		norm r | r < 0 = 4 + (r `mod` 4)
+		norm r = r
+
+		nr = norm r
+		rotateCoord coord = foldl rotateOnce coord [1..nr]
 		rotateOnce (sx, sy) _ = (-sy, sx)
 
 translate :: Int -> Int -> Shape -> Shape
 translate x y shape = map translateCoord shape
 	where
 		translateCoord (sx, sy) = (x + sx, y + sy)
+
+transform :: Int -> Int -> Int -> (Shape -> Shape)
+transform x y r = (translate x y) . (rotate r)
 
 sL :: Shape
 sL = [(0, 0), (0, 1), (0, 2), (1, 2)]
@@ -113,21 +130,34 @@ sT = [(0, 0), (1, 0), (2, 0), (1, 1)]
 -- main
 -------
 
+decorateWall :: Wall -> Wall
+decorateWall wall = case decorate of
+							Just wall' -> wall'
+							Nothing    -> wall
+	where
+		decorate :: Maybe Wall
+		decorate =
+			addSafe sJ  8 3 3 Red $ 
+			addSafe sI  6 4 0 Yellow $ 
+			addSafe sZ  6 1 1 Red $
+			addSafe sS  6 3 0 White $ 
+			addSafe sJ  7 1 3 Yellow $
+			addSafe sL  4 2 0 White $
+			addSafe sO 10 1 0 White $
+			addSafe sT 11 5 3 Yellow $ 
+			addSafe sS 13 4 3 Red $
+			Just wall
+
+
+
 main :: IO ()
 main = do
-	let wall = add sJ  8 3 3 Red $ 
-			   add sI  6 4 0 Yellow $ 
-			   add sZ  6 1 1 Red $
-			   add sS  6 3 0 White $ 
-			   add sJ  7 1 3 Yellow $
-			   add sL  4 2 0 White $
-			   add sO 10 1 0 White $
-			   add sT 11 5 3 Yellow $ 
-			   add sS 13 4 3 Red $
+	let wall = decorateWall $
 			   setColor 1 1 Yellow $ 
 			   setColor 2 2 Red $ 
 			   setColor 1 3 White $ 
 			   createWall 30 7 Nothing	
-	writeFile "/tmp/tiles.html" $ wall2html wall
+	let wall' =  wall
+	writeFile "/tmp/tiles.html" $ wall2html wall'
 	--print $ wall
 	--print $ wall2html wall
